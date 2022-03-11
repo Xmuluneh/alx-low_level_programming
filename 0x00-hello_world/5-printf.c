f7b737c734a44d49fb154cf80d8b478fd787e730
Xmuluneh <mulabr673@gmail.com>
2022-03-11 23:42:05 +0300

    hello test

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *My hello printf task
+ *Return:return 1 aftwer program excute
+*/
+int main(void)
+{
+printf("with proper grammer , but the outcome is apiece of art,\n"
+return (0);
+}
+ 
