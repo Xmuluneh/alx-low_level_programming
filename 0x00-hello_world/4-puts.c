7fbc79b615eba3efbe52b7521b04cc3ea7bd4628
MULUNEH ABRHAM GEBREMARIAM <mulabr673@gmail.com>
2022-03-11 17:19:01 -1200

    c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@ include<stdio.h>
  */
 int main(void)
 {
- Puts(\"Programming is like building a multilingual puzzle");
+ Puts("\"Programming is like building a multilingual puzzle");
  return (0);
 }
