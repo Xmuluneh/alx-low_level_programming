594804e390e728e121193df9425cace12ae4fac5
Xmuluneh <mulabr673@gmail.com>
2022-03-13 13:36:44 +0300

    hello test

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  *main - Entry point
  *My hello,printf task
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-printf("wuth proper grammer, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
  
