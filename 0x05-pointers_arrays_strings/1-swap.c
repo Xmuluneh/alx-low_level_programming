bf8bb0de78c24f4829a4772d42b66a70f1b6f0ca
Xmuluneh <mulabr673@gmail.com>
2022-03-21 13:20:15 +0300

    swapp the two number

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - Swap values of the parametr passed
+ *
+ * @a : The first par.
+ *
+ * @b : The seconf par.
+ *
+ */
+void swap_int(int *a, int *b)
+{
+int c ;
+int d ;
+c = *a;
+d = *b;
+*a =d;
+*b =c;
+}
+
