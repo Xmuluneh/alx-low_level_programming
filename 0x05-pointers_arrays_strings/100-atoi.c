fde3898ad7b6342872ae9047aafc1a9faaf53bea
Xmuluneh <mulabr673@gmail.com>
2022-03-21 15:13:51 +0300

    new look

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -19,39 +19,4 @@ int _atoi(char *s)
 	number *= neg;
 	return (number);
 }
-/**
- * binary_to_uint - converts binary to unisigned int
- * @b: input string
- * Return: converted number or 0 if it fails
- */
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int num, last, base = 1, decimal = 0, count = 0;
-
-	char *temp = (char *) b;
-
-	if (b == NULL)
-		return (0);
-
-	while (*temp)
-	{
-		if (*temp != '1' && *temp != '0')
-			return (0);
-		count++;
-		temp++;
-	}
-
-	num = _atoi((char *)b);
-
-	while (num)
-	{
-		last = num % 10;
-		num /= 10;
-		decimal += last * base;
-		base *= 2;
-	}
-
-	return (decimal);
-
-}
 
