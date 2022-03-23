dfc785a01e715903f0a214453532cb30fe61805c
Xmuluneh <mulabr673@gmail.com>
2022-03-23 18:18:23 +0300

    conc the string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,19 @@
 */
 char *_strcat(char *dest, char *src)
 {
-strcat(dest,src);
-return (dest);
+int i, n, m;
+
+	for (m = 0; dest[m] != '\0'; m++)
+	{
+	}
+	for (n = 0; src[n] != '\0'; n++)
+	{
+	}
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+	dest[m + i] = src[i];
+	}
+	return (dest);
+
 }
  
