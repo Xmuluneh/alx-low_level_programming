4fbc24b7d7ba2a2e1ecf159a19a875cd67ef47b7
Xmuluneh <mulabr673@gmail.com>
2022-03-23 18:34:09 +0300

    string conc

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,31 @@
-#include <main.h>
-#include <string.h>
-/*
- * _strncpy - copies up to n chracters from the string pointed to 
- *
- *
- *
- *
- *
- * Return : dest
-*/
+#include "main.h"
+
+/**
+ * _strncpy -  function that copies a string..
+ * @src: cadena a copiar
+ * @dest: destino de la cadena.
+ * @n: number.
+ * Return: void
+ */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-strncpy(dest, src, n);
-return (dest);
+	int i;
+
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return (dest);
 }
- 
+
