62cb53767de36243d5612715dec3668985e8d742
Xmuluneh <mulabr673@gmail.com>
2022-03-23 15:38:09 +0300

    conc string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _stecat -Concatenates teo string together
+ * @dest: First string, second string is appended to it
+ * @src: Second string , is is apprnded to the first
+ * Return : dest
+*/
+char *_strcat(char *dest, char *src)
+{
+strcat(dest,src);
+return (dest);
+}
+ 
