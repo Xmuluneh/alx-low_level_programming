1fbe09c82b5acfb0bf0154150cb172b54a6a4d8d
Xmuluneh <mulabr673@gmail.com>
2022-03-23 16:28:27 +0300

    copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include <main.h>
+#include <string.h>
+/*
+ * _strncpy - copies up to n chracters from the string pointed to 
+ *
+ *
+ *
+ *
+ *
+ * Return : dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+return (dest);
+}
+ 
