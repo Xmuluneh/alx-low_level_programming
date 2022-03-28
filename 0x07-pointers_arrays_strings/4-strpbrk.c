9a579b2d67ba19700fc3473fb012a54bb74b2c56
Xmuluneh <mulabr673@gmail.com>
2022-03-28 12:51:38 +0300

    man

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strpbrk - copies memory area.
+ * @s: the address of memory to print
+ * @accept: the source of the memory to print
+ * Return: Nothing.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *(s + i) != '\0' ; i++)
+	{
+		for (j = 0; *(accept + j) != '\0' ; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				s = (s + i);
+				return (s);
+			}
+		}
+	}
+	return (NULL);
+
+}
