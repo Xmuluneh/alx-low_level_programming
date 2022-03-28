3d4a1f193213731f496a640607948f5f59ad80b6
Xmuluneh <mulabr673@gmail.com>
2022-03-28 13:19:51 +0300

    muluneh

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,21 +1,21 @@
-#include "main.h" 
-#include <stdio.h>
+#include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte.
- * @b: the address of memory to print
- * @s: the size of the memory to print
- * @n: the size of the characters
- * Return: Nothing.
- */
-
+* _memset - fills memory with a constant byte
+* @s: pointer to memory
+* @b: byte to fill with
+* @n: number of bytes to be filled
+*
+* Return: returns pointer to memory area s
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		s[i] = b;
+		i++;
 	}
 	return (s);
 }
