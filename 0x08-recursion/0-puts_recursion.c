6960ab236c320e13819315e26ecee83f6aae3af1
Xmuluneh <mulabr673@gmail.com>
2022-03-30 11:45:15 +0300

    hello

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,6 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	puts(s);
+         int i=;
+      if (s[i] != '\0')
+      {
+           _putchar(s[i]);
+          _puts_recursion(&s[i + 1]);
+      }
+      else
+         _putchar('\n');
 }
 
