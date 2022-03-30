1b6da87fff2aace323ca0f7f86f86676ef70b034
Xmuluneh <mulabr673@gmail.com>
2022-03-30 12:24:52 +0300

    palindrom

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,54 @@
 #include "main.h"
+/**
+ *_strlen_recursion - checks the string length
+ *@s: string
+ *Description: firnd string length using recursion
+ *Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+
+	if (s[1] == '\0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (_strlen_recursion(&s[1]) + 1);
+	}
+}
 
 /**
- * is_palindrome - checks if a string is a palindrome or not.
- * @s: the string to scan
- *
- * Return: 1 if palindrome, else 0 assuming "" is palindrome.
+ *palind - checks palindromity
+ *@s: is a string
+ *@n: size of string
+ *@i: count through string
+ *Description: determine if string is palindrome
+ *Return: 1 if palindrome 0 if not
+ */
+int palind(char *s, int n, int i)
+{
+	if (n <= i)
+		return (1);
+
+	if (s[i] != s[n])
+		return (0);
+
+	return (palind(s, n - 1, i + 1));
+}
+
+/**
+ *is_palindrome - checks weither palindrome or not
+ *@s: is the string to be checked
+ *Description: determines if string is palindrome
+ *Return: 1 if it is palindrome 0 if it is not
  */
 int is_palindrome(char *s)
 {
-	if (!s)
-		return (1);
-	return (check_if_palindrome(s, 0, find_strlen(s)));
+	return (palind(s, _strlen_recursion(s) - 1, 0));
+
 }
-
-/**
- * find_strlen - finds the length of a string
- * @s: the string whose length is sought
- *
- * Return: the length of s
- */
-int find_strlen(char *s)
-{
-	if (*s != '\0')
-		return (1 + find_strlen(s + 1));
-	else
-		return (0);
-}
-
-/**
- * check_if_palindrome - check if string is a palindrome
- * @s: the string to be checked
- * @i: index
- * @len: the length of the s
- *
- * Return: 1 if palindrome,  else 0.
- */
-int check_if_palindrome(char *s, int i, int len)
-{
-	if (s[i] == s[len / 2])
-		return (1);
-	if (s[i] == s[len - i - 1])
-		return (check_if_palindrome(s, i + 1, len));
-
-	return (0);
-}
-
+C
+xx}
