bafaa3077d570f9c1a7a6fd5ca79cbb7a472a1d4
Xmuluneh <mulabr673@gmail.com>
2022-04-01 23:12:55 +0300

    c-file

---
+++ 0x09-static_libraries/4-strpbrk.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strchr - copies memory area.
+ * @s: the address of memory to print
+ * @accept: the source of the memory to print
+ * Return: Nothing.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+	return (0);
+}
+
