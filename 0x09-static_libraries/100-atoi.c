f678f8e971cbfd2b48001dfe0c22d9a5d6631949
Xmuluneh <mulabr673@gmail.com>
2022-04-01 22:42:57 +0300

    c-file

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *_isalpha - progam return1 for alpha case
+ *@c: Value of input
+ *Return: 1 or 0
+ *
+ */
+
+int _atoi(char *s)
+{
+	return (0);
+}
+
