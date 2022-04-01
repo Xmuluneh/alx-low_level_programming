7ae991b6408c0c91d2a9005007b8916eec2da931
Xmuluneh <mulabr673@gmail.com>
2022-04-01 22:48:48 +0300

    c-file

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen - returns the length of a string.
+ *@s : pointer
+ * Return: i value
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0';)
+	{
+	i++;
+	}
+	return (i);
+}
+
