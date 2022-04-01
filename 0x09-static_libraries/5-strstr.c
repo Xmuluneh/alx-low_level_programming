cffeb4586d5044ea305949165ce3710579c317f4
Xmuluneh <mulabr673@gmail.com>
2022-04-01 23:15:34 +0300

    c-file

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *_isalpha - progam return1 for alpha case
+ *@c: Value of input
+ *Return: 1 or 0
+ *
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	return (0);
+}
+
