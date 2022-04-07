5618347182177715a4eaab4940c0b9b8655f246f
Xmuluneh <mulabr673@gmail.com>
2022-04-07 23:07:43 +0300

    hello

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -2,42 +2,44 @@
 #include <stdlib.h>
 
 /**
- * _realloc - reallocates a memory block using malloc and free.
- * @ptr:  is a pointer to the memory previously allocated with a
- * call to malloc: malloc(old_size).
- * @old_size: is the size, in bytes, of the allocated space for ptr
- * @new_size: is the new size, in bytes of the new memory block
- *
- * Return: pointer to reallocated memory, if conditions met,
- * else, return based on specific conditions
+ * _realloc - reallocate memory using malloc
+ * @ptr: old allocated memory
+ * @old_size: size of ptr
+ * @new_size: new allocated memory size
+ * Return: new_ptr
  */
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *rmemb;
-	char *p;
-	unsigned int i;
+char *old_ptr;
+char *new_ptr;
+unsigned int count;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (!new_size && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	if (ptr == NULL)
-	{
-		rmemb = malloc(new_size);
-		if (rmemb == NULL)
-			return (NULL);
-	}
-	if (new_size > old_size)
-	{
-		p = ptr;
-		rmemb = malloc(new_size);
-		for (i = 0; i < old_size; i++)
-			rmemb[i] = p[i];
-		free(ptr);
-	}
-	return (rmemb);
+old_ptr = (char *)ptr;
+
+if (new_size == old_size)
+return (ptr);
+
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+
+new_ptr = malloc(new_size);
+
+if (ptr == NULL)
+return (new_ptr);
+
+if (new_size < old_size)
+for (count = 0; count < new_size; count++)
+new_ptr[count] = old_ptr[count];
+
+if (new_size > old_size)
+for (count = 0; count < old_size; count++)
+new_ptr[count] = old_ptr[count];
+
+free(ptr);
+return (new_ptr);
 }
 
