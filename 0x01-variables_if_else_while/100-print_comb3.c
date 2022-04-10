4d8e83eb88a40efa1f6c05582b0bd5366b711c8f
Xmuluneh <mulabr673@gmail.com>
2022-04-10 11:37:59 +0300

    combine two num

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,9 +15,11 @@ int main(void)
 	{
 		for (j = '0'; j <= '9'; j++)
 		{
+			if (i < j)
+			{
 			putchar(i);
 			putchar(j);
-			if (i < '9' || j < '9')
+			if (i < '8' || j < '9')
 			{
 				putchar(44);
 				putchar(32);
@@ -25,6 +27,7 @@ int main(void)
 			else
 			{
 			putchar('\n');
+			}
 			}
 		}
 	}
