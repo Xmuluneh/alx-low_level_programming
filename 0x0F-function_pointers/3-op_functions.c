9f8d0625713204b6fee1eef4994697dfcff744fb
Xmuluneh <mulabr673@gmail.com>
2022-04-13 23:57:48 +0300

    hello

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,68 +1,57 @@
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- * op_add - sum operator
- *
- * @a: The first number
- * @b: The second number
- * Return: te result
+ * op_add - addition function
+ * @a: first operand
+ * @b: second operand
+ * Return: the sum of a and b
  */
-
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
- * op_sub - sum operator
- *
- * @a: The first number
- * @b: The second number
- * Return: te result
+ * op_sub - subtraction function
+ * @a: first operand
+ * @b: second operand
+ * Return: the difference btw a and b
  */
-
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 
 /**
- * op_mul - sum operator
- *
- * @a: The first number
- * @b: The second number
- * Return: te result
+ * op_mul - multiplication function
+ * @a: first operand
+ * @b: second operand
+ * Return: the product of a and b
  */
-
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 
 /**
- * op_div - sum operator
- *
- * @a: The first number
- * @b: The second number
- * Return: te result
+ * op_div - division function
+ * @a: first operand
+ * @b: second operand
+ * Return: the quotient of a÷b
  */
-
 int op_div(int a, int b)
 {
-return (a / b);
+	return (a / b);
 }
 
 /**
- * op_mod - sum operator
- *
- * @a: The first number
- * @b: The second number
- * Return: te result
+ * op_mod - modulus function
+ * @a: first operand
+ * @b: second operand
+ * Return: the remainder of a÷b
  */
-
 int op_mod(int a, int b)
 {
-return (a % b);
+	return (a % b);
 }
 
