32f06216a0cc1da0942ac3b1459f917c808ee883
Xmuluneh <mulabr673@gmail.com>
2022-04-13 21:17:50 +0300

    function

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ *get_op_func - Struct op
+ *@s: The operator
+ *Return: the operator
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+{op_add, "+"},
+{op_sub, "-"},
+{op_mul, "*"},
+{op_div, "/"},
+{op_mod, "%"},
+{NULL, NULL}
+};
+
+int i;
+
+i = 0;
+
+while (i < 6)
+{
+	if (s[0] == ops[i].op[0])
+	{
+		return (ops[i].f);
+	}
+i++;
+}
+return (0);
+}
+
