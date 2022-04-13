e9245d464d88a4430a786f2a372d36100d1ec972
Xmuluneh <mulabr673@gmail.com>
2022-04-13 23:53:20 +0300

    hello

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,37 +1,42 @@
-#include <stdlib.h>
 #include "3-calc.h"
 
 /**
- *main - main
- *@argc: counts strings
- *@argv: pointer to strings
- *Return: void
+ * main - the starting function
+ * @argc: the number of passed arguments including the program's name
+ * @argv: an array whose elements are the passed arguments
+ *
+ * Return: Always 0.
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int num1, num3;
-num1 = atoi(argv[1]);
-
-num3 = atoi(argv[3]);
+	int x, y;
+	char o;
+	int (*op_func)(int, int);
 
 	if (argc != 4)
-		{
-		printf("Error\n");
-		exit(98);
-		}
-	if ((argv[2]) == NULL || (argv[2][1]) != '\0')
-		{
-		printf("Error\n");
-		exit(99);
-		}
-
-	if (num3 == 0 && ((argv[2][0]) == '%' || (argv[2][0]) == '/'))
 	{
 		printf("Error\n");
-		exit(100);
+		return (98);
 	}
-	printf("%d\n", get_op_func(argv[2])(num1, num3));
+
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	o = *argv[2];
+
+	op_func = get_op_func(argv[2]);
+
+	if (!op_func)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((o == '/' || o == '%') && y == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+	printf("%d\n", op_func(x, y));
 	return (0);
 }
 
