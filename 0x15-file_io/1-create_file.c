89ff1319891b883440a729b9c319db65fb67b834
Xmuluneh <mulabr673@gmail.com>
2022-05-02 21:12:17 +0300

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,43 +1,34 @@
 #include "main.h"
 
 /**
- * create_file - check the code for Holberton School students.
- * @filename: file name
- * @text_content: text
- * Return: int.
+ * create_file - create a new file, if the file does not exist
+ * @filename: the name of the file to be created
+ * @text_content: the contents to be stored in the file
+ *
+ * Return: 1 if success, else -1
  */
-
-
 int create_file(const char *filename, char *text_content)
 {
-int fd, i;
-ssize_t copiar;
-	if (filename == NULL)
+	int fd;
+	int len = 0, i;
+
+	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
+	fd = open(filename, O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 00600);
+	if (fd < 0)
+		return (-1);
+
+	if (text_content)
 	{
-		i = 0;
+		while (text_content[len])
+			len++;
+		i = write(fd, text_content, len);
+		if (len != i)
+			return (-1);
 	}
-	else
-	{
-	for (i = 0; *(text_content + i) != '\0'; i++)
-	;
-	}
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-
-	if (fd == -1)
-		return (-1);
-
-		copiar = write(fd, text_content, i);
-
-	if (copiar == -1)
-		return (-1);
-
-		close(fd);
-
+	close(fd);
 	return (1);
 }
 
