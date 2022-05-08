0375b634af469db7ed9e7e0d491639b3e0b472e6
Xmuluneh <mulabr673@gmail.com>
2022-05-08 16:42:40 +0300

    print the lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @digit: parameter of innteger
+ * Return: the last digit
+ */
+int print_last_digit(int digit)
+{
+	digit = digit % 10;
+
+	if (digit < 0)
+	{
+		digit = -digit;
+	}
+	_putchar(digit + '0');
+	return (digit);
+}
+
