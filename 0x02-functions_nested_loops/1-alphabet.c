912cf4836775eaa67399b3f149e698a7f5fbc75f
Xmuluneh <mulabr673@gmail.com>
2022-05-08 16:27:36 +0300

    print alphabets in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lower case alphabet
+ *
+ **/
+
+void print_alphabet(void)
+{
+
+	char lower_case_letter = 'a';
+
+	while (lower_case_letter <= 'z')
+	{
+		_putchar(lower_case_letter);
+		lower_case_letter++;
+	}
+	_putchar('\n');
+}
