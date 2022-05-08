6b3778ed09f62ab6ccfb720ceb19dc69082333c2
Xmuluneh <mulabr673@gmail.com>
2022-05-08 16:30:13 +0300

    print the alpahabet ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabet in 10 rows
+ */
+
+void print_alphabet_x10(void)
+{
+	char j;
+	int i = 0;
+
+	while (i < 10)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+
+		_putchar('\n');
+		i++;
+	}
+}
+
