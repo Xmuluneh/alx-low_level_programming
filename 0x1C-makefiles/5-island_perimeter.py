953a44e39f077af3e69bf252e8fe8397b2cfb47b
Xmuluneh <mulabr673@gmail.com>
2022-08-17 18:06:35 +0300

    Perimete of island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,30 @@
 #!/usr/bin/python3
+"""Module calculate the perimeter of an island in  a grid"""
 def island_perimeter(grid):
-    """uyagdyugusa"""
-    
+    """Return the perimeter of the island in grid
+       Args:
+            -grid: the first parameter
+    """
+    per_meter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                per_meter += numberOfWater(grid, i, j)
+
+    return per_meter
+def numberOfWater(grid, i, j):
+    """Returns the number of water neighbors a cell has in a grid."""
+
+    number = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        number += 1
+    if j <= 0 or not grid[i][j - 1]:
+        number += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        number += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        number += 1
+
+    return number
+
